d440e9d8b00001030301ed2e51a4b9b0fba3e791
Aellantis <Ashleyyung.1231@gmail.com>
2025-01-26 22:17:21 -0800

    finished dictionary_words and decided to decrease the indent spacing to be consistent

--- Code/python_quote.py
+++ Code/python_quote.py
@@ -5,9 +5,9 @@ quotes = ("It's just a flesh wound.",
           "THIS IS AN EX-PARROT!!")
 
 def random_python_quote():
-    rand_index = random.randint(0, len(quotes) - 1)
-    return quotes[rand_index]
+  rand_index = random.randint(0, len(quotes) - 1)
+  return quotes[rand_index]
 
 if __name__ == '__main__':
-    quote = random_python_quote()
-    print(quote)
+  quote = random_python_quote()
+  print(quote)
