82a62b2d45726eb23cba984cdae4aedaa48eeb86
Aellantis <Ashleyyung.1231@gmail.com>
2025-01-29 22:49:06 -0800

    Completed histogram + added Alice in wonderland txt file

---
+++ Code/histogram.py
@@ -0,0 +1,19 @@
+import re
+
+def histogram (source_text):
+    with open(source_text, 'r') as file:
+        text = file.read().lower() 
+        words = re.findall(r"\b[a-zA-Z']+\b", text) 
+        return words  
+    
+
+def unique_words (histogram):
+    return len(set(histogram))
+
+
+def frequency (word, histogram):
+    return histogram.count(word.lower().strip())
+
+text = histogram('data/alice_in_wonderland.txt')
+print(f"Unique words: {unique_words(text)}")
+print(f"Frequency: {frequency("wonderland", text)}")
