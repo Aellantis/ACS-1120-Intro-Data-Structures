7336a4e1f6ba02d84730d338ef83f1cd79892405
Aellantis <Ashleyyung.1231@gmail.com>
2025-02-19 09:46:05 -0800

    Setting up for render. Added code to app.py

--- Code/app.py
+++ Code/app.py
@@ -1,17 +1,21 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
-
+from .histogram import histogram
+import random
 
 app = Flask(__name__)
 
-# TODO: Initialize your histogram, hash table, or markov chain here.
-# Any code placed here will run only once, when the server starts.
+hist = histogram("Code/data/alice_in_wonderland.txt")
 
 
 @app.route("/")
 def home():
-    """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+  words = list(hist.keys()) # getting all words from histo as a list
+  sentence_length = random.randint(20, 30)
+  # generating rand words & joining w spaces
+  random_words = [words[random.randint(0, len(words) - 1)] for _ in range(sentence_length)]
+  sentence = " ".join(random_words) 
+  return f"<p>{sentence}</p>"
 
 
 if __name__ == "__main__":
